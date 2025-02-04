035e2d7c3a4c692c7cb402beb3caf75b8173464f
Руслан Усманов <ruslanusmanov029@gmail.com>
2025-02-05 00:33:07 +0300

    Описание изменений

--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -15,7 +15,7 @@ const SearchBar = ({ onSearch }) => {
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        placeholder="Введите название видео"
+        placeholder="Введите название питомца"
       />
       <button type="submit">Поиск</button>
     </form>
