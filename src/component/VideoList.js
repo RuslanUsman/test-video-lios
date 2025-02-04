c408e18a89b7cf4dcb4d0f980642811f6b8f4fe2
Руслан Усманов <ruslanusmanov029@gmail.com>
2025-02-04 23:19:52 +0300

    Описание изменений

--- src/component/VideoList.js
+++ src/component/VideoList.js
@@ -1,15 +1,40 @@
 // src/component/VideoList.js
-import React from 'react';
+import React, { useRef } from 'react';
 import ReactPlayer from 'react-player';
 import './VideoList.css';
 
 const VideoList = ({ videos, onSelect }) => {
+  const playerRefs = useRef([]);
+
+  const handleFullscreen = (index) => {
+    const player = playerRefs.current[index];
+    if (player) {
+      const videoElement = player.getInternalPlayer();
+      if (videoElement.requestFullscreen) {
+        videoElement.requestFullscreen();
+      } else if (videoElement.mozRequestFullScreen) { /* Firefox */
+        videoElement.mozRequestFullScreen();
+      } else if (videoElement.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        videoElement.webkitRequestFullscreen();
+      } else if (videoElement.msRequestFullscreen) { /* IE/Edge */
+        videoElement.msRequestFullscreen();
+      }
+    }
+  };
+
   return (
     <div className="video-list">
       {videos.map((video, index) => (
-        <div key={index} className="video-item" onClick={() => onSelect(video)}>
-          <ReactPlayer url={video.url} controls width="100%" height="100%" />
+        <div key={index} className="video-item">
+          <ReactPlayer
+            ref={(el) => (playerRefs.current[index] = el)}
+            url={video.url}
+            controls
+            width="100%"
+            height="100%"
+          />
           <div className="video-title">{video.title}</div>
+          <button onClick={() => handleFullscreen(index)}>Полный экран</button>
         </div>
       ))}
     </div>
